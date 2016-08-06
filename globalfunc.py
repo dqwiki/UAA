a3fcc7a2477a4560f2775c8f693c5063350f0ff9
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2016-08-06 00:24:39 -0400

    force edits before posting

--- globalfunc.py
+++ globalfunc.py
@@ -14,7 +14,7 @@ else:sys.path.append(localconfig.linuxpath)
 import pywikibot
 from pywikibot.data import api
 
-useWiki= pywikibot.Site('en','wikipedia')
+useWiki = pywikibot.Site('en', 'wikipedia')
 
 def callAPI(params):
     req = api.Request(useWiki, **params)
@@ -22,9 +22,9 @@ def callAPI(params):
 
 def currentTime():
 	time = str(datetime.utcnow())
-	time = time.replace(' ','T')
-	time = time.replace('-','')
-	time = time.replace(':','')
+	time = time.replace(' ', 'T')
+	time = time.replace('-', '')
+	time = time.replace(':', '')
 	time = time.split('.')[0]
 	time = time + 'Z'
 	return time
@@ -57,13 +57,13 @@ def checkBlocked(user):
                         return True
                 except:return False
         except:return None
-def checkRegisterTime(user, maxDays,advanced):
+def checkRegisterTime(user, maxDays, advanced):
         """Returns True if the given user is more than maxDays old, else False."""
         maxSeconds = maxDays * 24 * 60 * 60
-        params = {"action": "query", "list": "users", "ususers": user, "format": "json", "usprop": "registration","rawcontinue":"1"}
+        params = {"action": "query", "list": "users", "ususers": user, "format": "json", "usprop": "registration", "rawcontinue":"1"}
         result = callAPI(params)
         try:reg = result["query"]["users"][0]["registration"]
-        except:return [False,False]
+        except:return [False, False]
         then = time.strptime(reg, "%Y-%m-%dT%H:%M:%SZ")
         now = time.gmtime()
         thenSeconds = time.mktime(then)
@@ -78,17 +78,17 @@ def checkRegisterTime(user, maxDays,advanced):
                 return False
 
 def searchlist(line, listtype):
-    try:line=line.decode("utf-8")
-    except:noNeedToTryAndPlayWithEncoding = True #not a real var
+    try:line = line.decode("utf-8")
+    except:noNeedToTryAndPlayWithEncoding = True  # not a real var
     if line == "":return
     if listtype == "bl":
-        i=0
+        i = 0
         while i < len(bl):
             if bl[i].lower().split(":")[0] != "":check = re.search(bl[i].lower().split(":")[0], line.lower())
             else: check = None
             if not (check == "None" or check == None):
                 return [True, bl[i].split(":")[0], ' '.join(bl[i].split(":")[1:])]
-            i = i+1
+            i = i + 1
         return [False, None, None]
     if listtype == "wl":
         for entry in wl:
@@ -96,8 +96,8 @@ def searchlist(line, listtype):
                 return True
         return False
     if listtype == "sl":
-        i=0
-        while i < len(sl):#can be omptimized with for statement
+        i = 0
+        while i < len(sl):  # can be omptimized with for statement
             if re.search(".", sl[i]) != None:
                 stringline = sl[i].split(":")[1]
                 stringline = stringline.split(" ")
@@ -112,12 +112,12 @@ def searchlist(line, listtype):
                                 if not wlcheck:
                                     if "LABEL" in flags:
                                         note = flags.split("LABEL(")[1].split(")")[0]
-                                        return [False, 'Used '+everyexpr.lower()+ ' instead of '+sl[i].split(":")[0]+' attempting to skip filter: '+note + '. Violating string: '+newline,blslcheck[2]]
+                                        return [False, 'Used ' + everyexpr.lower() + ' instead of ' + sl[i].split(":")[0] + ' attempting to skip filter: ' + note + '. Violating string: ' + newline, blslcheck[2]]
                                     else:
-                                        return [False, 'Used '+everyexpr.lower()+ ' instead of '+sl[i].split(":")[0]+' attempting to skip filter: '+blslcheck[1] + '. Violating string: '+newline,blslcheck[2]]
+                                        return [False, 'Used ' + everyexpr.lower() + ' instead of ' + sl[i].split(":")[0] + ' attempting to skip filter: ' + blslcheck[1] + '. Violating string: ' + newline, blslcheck[2]]
                                 else:
                                         return [True, None, None]
-            i = i+1
+            i = i + 1
         matchnum = 0
         for eachline in sl:
                 if eachline == "":continue
@@ -125,46 +125,46 @@ def searchlist(line, listtype):
                 splitline = splitline.split(" ")
                 for entry in splitline:                        
                         if entry in line:
-                                if not re.search('[a-z]|[A-Z]|[0-9]',entry) == None:continue
-                                matchnum = matchnum +1
-        if matchnum > 1:return [False, 'Attempting to skip filters using multiple similiar charecters','LOW_CONFIDENCE,NOTE(Multiple characters like ν and ә can be contained in the same phrase, this rule detects when one or more occurs)']
+                                if not re.search('[a-z]|[A-Z]|[0-9]', entry) == None:continue
+                                matchnum = matchnum + 1
+        if matchnum > 1:return [False, 'Attempting to skip filters using multiple similiar charecters', 'LOW_CONFIDENCE,NOTE(Multiple characters like ν and ә can be contained in the same phrase, this rule detects when one or more occurs)']
         return True
-def checkUser(user,waittilledit,noEdit):
+def checkUser(user, waittilledit, noEdit):
         bltest = searchlist(user, "bl")
         try:line = str(bltest[1])
         except UnicodeEncodeError:
                 line = bltest[1]
         except:
-                post(user,"This bot does not support the encoding in this username or filter. Please consider reporting this to my master. Tripped on: " + bltest[1],"LOW_CONFIDENCE",False)
-                trace = traceback.format_exc() # Traceback.
-                print trace # Print.
+                post(user, "This bot does not support the encoding in this username or filter. Please consider reporting this to my master. Tripped on: " + bltest[1], "LOW_CONFIDENCE", False)
+                trace = traceback.format_exc()  # Traceback.
+                print trace  # Print.
                 return
         flags = str(bltest[2])
         if searchlist(user, "wl"):
                 return
         if bltest[0]:
                 if noEdit:
-                        #print 'No edit - 1' + str(bltest[1]) +" "+ str(bltest[2])
+                        # print 'No edit - 1' + str(bltest[1]) +" "+ str(bltest[2])
                         return 
                 else:
-                        try:return post(user,str(bltest[1]),str(bltest[2]),str(waittilledit))
+                        try:return post(user, str(bltest[1]), str(bltest[2]), str(waittilledit))
                         except:return
         if "NO_SIM_MATCH" in flags:return
         slcheck = searchlist(user, "sl")
-        if slcheck == True:a=1
+        if slcheck == True:a = 1
         elif waittilledit != False and 'WAIT_TILL_EDIT' in str(slcheck[2]):waittilledit = True
         try:
                 if not slcheck[0] and not bltest[0]:
                         if noEdit:
-                                #print "No edit - 2 "+str(slcheck[1]) +" "+ str(slcheck[2])
+                                # print "No edit - 2 "+str(slcheck[1]) +" "+ str(slcheck[2])
                                 return
-                        return post(user,str(slcheck[1]),str(slcheck[2]),str(waittilledit))
+                        return post(user, str(slcheck[1]), str(slcheck[2]), str(waittilledit))
         except:
                 if not slcheck and not bltest[0]:
                         if noEdit:
-                                #print "No edit - 3"+str(slcheck[1]) +" "+ str(slcheck[2])
+                                # print "No edit - 3"+str(slcheck[1]) +" "+ str(slcheck[2])
                                 return
-                        return post(user,str(slcheck[1]),str(slcheck[2]),str(waittilledit))
+                        return post(user, str(slcheck[1]), str(slcheck[2]), str(waittilledit))
         return
 def main():
         params = {'action': 'query',
@@ -182,8 +182,8 @@ def main():
         for entry in reg:
                 try:user = entry["user"]
                 except KeyError:
-                        #Placeholder for OS'd users
-                        oversighted=True
+                        # Placeholder for OS'd users
+                        oversighted = True
                         continue
                 if user == "":continue
                 checkUser(user, True, False)
@@ -204,23 +204,23 @@ def runDry():
                 if user == "":continue
                 checkUser(user, True, True)
 def post(user, match, flags, restrict):
-        summary = "[[User:"+localconfig.botname+"|"+localconfig.botname+"]] "+ localconfig.primarytaskname +" - [[User:"+user+"]] ([[Special:Block/"+user+"|Block]])"
+        summary = "[[User:" + localconfig.botname + "|" + localconfig.botname + "]] " + localconfig.primarytaskname + " - [[User:" + user + "]] ([[Special:Block/" + user + "|Block]])"
         site = pywikibot.getSite()
         pagename = localconfig.postpage
         page = pywikibot.Page(site, pagename)
         pagetxt = page.get()
         if user in pagetxt:
                 return
-        text = "\n\n*{{user-uaa|1="+user+"}}\n"
+        text = "\n\n*{{user-uaa|1=" + user + "}}\n"
         if "LOW_CONFIDENCE" in flags:
                 text = text + "*:{{clerknote}} There is low confidence in this filter test, please be careful in blocking. ~~~~\n"
-        if "WAIT_TILL_EDIT" in flags and restrict != False:#If waittilledit override it not active, aka first run
-                edited = getEditCount(user)
-                if edited == None:
-                        return#Skip user, probally non-existant
-                if edited == False:
-                        waitTillEdit(user)#Wait till edit, user has not edited
-                        return#leave this indented, or it will not continue to report edited users
+        # "WAIT_TILL_EDIT" in flags and restrict != False:#If waittilledit override it not active, aka first run
+        edited = getEditCount(user)
+        if edited == None:
+                return  # Skip user, probally non-existant
+        if edited == False:
+                waitTillEdit(user)  # Wait till edit, user has not edited
+                return  # leave this indented, or it will not continue to report edited users
         if "LABEL" in flags:
                 note = flags.split("LABEL(")[1].split(")")[0]
                 if "skip" in match: note = match
@@ -236,18 +236,18 @@ def post(user, match, flags, restrict):
         if restrict == False:text + "*:{{done|Waited until user edited to post.}} ~~~~\n"
         if not checkBlocked(user):page.put(pagetxt + text, comment=summary)
 def waitTillEdit(user):
-        registertime=checkRegisterTime(user, 7,True)
+        registertime = checkRegisterTime(user, 7, True)
         if not registertime[1]:
                 return
         if registertime[0]:
                 checkUser(user, False, True)
                 return
-        summary = "[[User:DeltaQuadBot|DeltaQuadBot]] Task UAA listing - Waiting for [[User:"+user+"]] ([[Special:Block/"+user+"|Block]]) to edit"
+        summary = "[[User:DeltaQuadBot|DeltaQuadBot]] Task UAA listing - Waiting for [[User:" + user + "]] ([[Special:Block/" + user + "|Block]]) to edit"
         site = pywikibot.getSite()
         pagename = localconfig.waitlist
         page = pywikibot.Page(site, pagename)
         pagetxt = page.get()
-        text = "\n*{{User|" + user+"}}"
+        text = "\n*{{User|" + user + "}}"
         if text in pagetxt:
                 return
         page.put(pagetxt + text, comment=summary)
@@ -264,11 +264,11 @@ def postCurrentRun():
         page = pywikibot.Page(site, pagename)
         page.put(str(currentTime()), comment=summary)
 def cutup(array):
-    i=1
-    while i < len(array)-1:
+    i = 1
+    while i < len(array) - 1:
         try:
             while array[i][0] != ";":
-                i=i+1
+                i = i + 1
             array[i] = array[i].split(":")
             i = i + 1
         except:
@@ -284,10 +284,10 @@ def getlist(req):
         pagename = localconfig.simlist
     page = pywikibot.Page(site, pagename)
     templist = page.get()
-    templist = templist.replace("{{cot|List}}\n","")
-    templist = templist.replace("{{cot}}\n","")
-    templist = templist.replace("{{cob}}","")
-    if req != "wl":templist = templist.replace("\n","")
+    templist = templist.replace("{{cot|List}}\n", "")
+    templist = templist.replace("{{cot}}\n", "")
+    templist = templist.replace("{{cob}}", "")
+    if req != "wl":templist = templist.replace("\n", "")
     if req != "wl":templist = templist.split(";")
     if req == "wl":templist = templist.split("\n;")
     templistarray = cutup(templist)
@@ -304,36 +304,36 @@ def startAllowed(override):
                 runDry()
         if start == "Dry":
                 print "Notice - Running Checkwait.py only"
-                import checkwait #import as it's a py file
+                import checkwait  # import as it's a py file
                 return False
         else:
                 return False
 def checkWait():
-        newlist=""#blank variable for later
+        newlist = ""  # blank variable for later
         site = pywikibot.getSite()
         pagename = localconfig.waitlist
         page = pywikibot.Page(site, pagename)
         waiters = page.get()
-        waiters = waiters.replace("}}","")
-        waiters = waiters.replace("*{{User|","")
+        waiters = waiters.replace("}}", "")
+        waiters = waiters.replace("*{{User|", "")
         waiters = waiters.split("\n")
         for waiter in waiters:
-                if waiter == "":continue#Non-existant user
-                if checkRegisterTime(waiter, 7,False):continue
-                if checkBlocked(waiter):continue#If user is blocked, skip putting them back on the list.
-                if getEditCount(waiter) == True:#If edited, send them to UAA
-                        checkUser(waiter,False,False)
+                if waiter == "":continue  # Non-existant user
+                if checkRegisterTime(waiter, 7, False):continue
+                if checkBlocked(waiter):continue  # If user is blocked, skip putting them back on the list.
+                if getEditCount(waiter) == True:  # If edited, send them to UAA
+                        checkUser(waiter, False, False)
                         continue
-                if waiter in newlist:continue#If user already in the list, in case duplicates run over
-                #Continue if none of the other checks have issues with the conditions for staying on the waitlist
+                if waiter in newlist:continue  # If user already in the list, in case duplicates run over
+                # Continue if none of the other checks have issues with the conditions for staying on the waitlist
                 newlist = newlist + "\n*{{User|" + waiter + "}}"
-                #print "\n*{{User|" + waiter + "}}"
+                # print "\n*{{User|" + waiter + "}}"
         summary = localconfig.editsumwait
         site = pywikibot.getSite()
         pagename = localconfig.waitlist
         page = pywikibot.Page(site, pagename)
         pagetxt = page.get()
-        newlist = newlist.replace("\n*{{User|}}","")
+        newlist = newlist.replace("\n*{{User|}}", "")
         page.put(newlist, comment=summary)
 def pageCleanup():
         resolvedDatabase = ["{{UAA\|w}}",
@@ -373,28 +373,28 @@ def pageCleanup():
                             "{{UAA\|r}}",
                             "{{UAA\|rfcn}}"
                             ]
-        newlist=""#blank variable for later
-        rawnewlist=""
-        movelist=""
+        newlist = ""  # blank variable for later
+        rawnewlist = ""
+        movelist = ""
         site = pywikibot.getSite()
         pagename = localconfig.postpage
         page = pywikibot.Page(site, pagename)
         uaapage = page.get()
-        #print uaapage
+        # print uaapage
         if "{{adminbacklog" in uaapage:
-                adminbacklog=True
-                uaapage.replace("{{adminbacklog}}\n","")
-        else:adminbacklog=False
-        uaapage = uaapage.replace("==[[Wikipedia:UAA/BOT|Bot-reported]]==\n","")
+                adminbacklog = True
+                uaapage.replace("{{adminbacklog}}\n", "")
+        else:adminbacklog = False
+        uaapage = uaapage.replace("==[[Wikipedia:UAA/BOT|Bot-reported]]==\n", "")
         usergrid = uaapage.split("*{{user-uaa|1=")
         for cell in usergrid:
                 try:
                         declined = False
                         if cell == "":continue
                         user = cell.split("}}")[0]
-                        if "{{" in user:continue#Admin backlog template
-                        if checkBlocked(user):continue#If user is blocked, skip putting them back on the list.
-                        if checkRegisterTime(user, 14,False):continue#Requests over 14 days are removed for inaction
+                        if "{{" in user:continue  # Admin backlog template
+                        if checkBlocked(user):continue  # If user is blocked, skip putting them back on the list.
+                        if checkRegisterTime(user, 14, False):continue  # Requests over 14 days are removed for inaction
                         if user in newlist:continue
                         for entry in resolvedDatabase:
                                 if re.search(entry.lower(), cell.lower()) == None:
@@ -413,18 +413,18 @@ def pageCleanup():
                         if declined:continue
                         rawnewlist = rawnewlist + "\n" + user
                         newlist = newlist + "*{{user-uaa|1=" + ''.join(cell)
-                        #print user
+                        # print user
                 except:continue
-        ## UAA Bot page posting ##
+        # # UAA Bot page posting ##
         summary = localconfig.editsumclear
         site = pywikibot.getSite()
         pagename = localconfig.postpage
         page = pywikibot.Page(site, pagename)
         pagetxt = page.get()
-        if adminbacklog:newlist+"{{adminbacklog}}\n"
+        if adminbacklog:newlist + "{{adminbacklog}}\n"
         newlist = "==[[Wikipedia:UAA/BOT|Bot-reported]]==\n" + newlist
         page.put(newlist, comment=summary)
-        ## UAA Holding pen posting ##
+        # # UAA Holding pen posting ##
         site = pywikibot.getSite()
         pagename = localconfig.holdpage
         page = pywikibot.Page(site, pagename)
@@ -432,7 +432,7 @@ def pageCleanup():
         if movelist == "":return
         if time.strftime("%B") not in holdpage:
                 holdpage = holdpage + "\n==" + time.strftime("%B") + "=="
-        if time.strftime("%d") not in holdpage.split(time.strftime("%B"))[1]: #or time.strftime("%d").split("0")[1] not in holdpage.split(time.strftime("%B"))[1]:
+        if time.strftime("%d") not in holdpage.split(time.strftime("%B"))[1]:  # or time.strftime("%d").split("0")[1] not in holdpage.split(time.strftime("%B"))[1]:
                 holdpage = holdpage + "\n===" + time.strftime("%d") + "===\n"
         holdpage = holdpage + "\n" + movelist
         page.put(holdpage, comment=summary)
