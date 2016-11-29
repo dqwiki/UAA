d77f85134e628f658bcd4e62cf310f9d215d62c8
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2016-11-29 18:21:03 -0500

    Licence

--- globalfunc.py
+++ globalfunc.py
@@ -1,3 +1,22 @@
+"""
+(C) 2016 DeltaQuad (enwp.org/User:DeltaQuad)
+
+This file is part of DeltaQuadBot.
+
+DeltaQuadBot is free software: you can redistribute it and/or modify
+it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DeltaQuadBot is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+
+You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+along with DeltaQuadBot. If not, see <https://www.gnu.org/licenses/agpl.txt>.
+"""
+
 # -*- coding: utf-8 -*-
 from datetime import datetime
 import sys
